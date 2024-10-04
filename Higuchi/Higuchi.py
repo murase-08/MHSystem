cabb6de5a9193a32c2c7b7af83e5d0085dd8394e
Takuto Higuchi <takuto3014@gmail.com>
2024-10-04 20:06:40 +0900

    10/04

--- Higuchi/Higuchi.py
+++ Higuchi/Higuchi.py
@@ -14,13 +14,19 @@ def read_pdf(company_code, pdf_path):
                 tables = page.extract_tables()
                 for table in tables:
                     for row in table[1:]:
-                        data.append({
+                        if len(row) == 11:
+                         data.append({
                             "日付": row[0],
-                            "開始": row[1],
-                            "終了": row[2],
-                            "休憩時間": row[3],
-                            "労働時間": row[4],
-                            "時間外": row[5]
+                            "勤怠区分": row[1],
+                            "開始": row[2],
+                            "終了": row[3],
+                            "休憩時間": row[4],
+                            "労働時間": row[5],
+                            "時間外": row[6],
+                            "深夜": row[7],
+                            "法定休日": row[8],
+                            "法定休日深夜": row[9],
+                            "メモ": row[10]
                         })
             pdf_df = pd.DataFrame(data)
             print(pdf_df)
@@ -36,9 +42,13 @@ def read_pdf(company_code, pdf_path):
                 tables = page.extract_tables()
                 for table in tables:
                     for row in table[1:]:
-                        data.append({
+                         if len(row) >= 4:
+                          data.append({
                             "日付": row[0],
-                            "実働時間": row[1]
+                            "曜日": row[1],
+                            "出勤日": row[2],
+                            "実働時間": row[3],
+                            "業務内容": row[4],
                         })
             pdf_df = pd.DataFrame(data)
             print(pdf_df)
@@ -46,6 +56,7 @@ def read_pdf(company_code, pdf_path):
         read_tdisystem_pdf(pdf_path)
 
     # 株式会社システムサポートのPDF読み込み関数
+    # 未完成
     elif company_code == 3:
         def read_systemsupport_pdf(pdf_path):
             data = []
@@ -53,13 +64,16 @@ def read_pdf(company_code, pdf_path):
                 page = pdf.pages[0]
                 tables = page.extract_tables()
                 for table in tables:
-                    for row in table[1:]:
-                        data.append({
+                    for row in table[9:]:
+                        if len(row) >= 6:
+                         data.append({
                             "日": row[0],
-                            "開始": row[1],
-                            "終了": row[2],
-                            "休憩": row[3],
-                            "作業時間": row[4]
+                            "曜日": row[1],
+                            "開始": row[2],
+                            "終了": row[3],
+                            "休憩": row[4],
+                            "作業時間": row[5],
+                            "作業内容": row[6],
                         })
             pdf_df = pd.DataFrame(data)
             print(pdf_df)
@@ -74,13 +88,24 @@ def read_pdf(company_code, pdf_path):
                 page = pdf.pages[0]
                 tables = page.extract_tables()
                 for table in tables:
-                    for row in table[1:]:
-                        data.append({
+                     for row in table[3:]:
+                        if len(row) >= 15:
+                         data.append({
                             "日": row[0],
-                            "始業時間": row[1],
-                            "休憩": row[2],
-                            "勤務時間計": row[3],
-                            "平日残業": row[4]
+                            "曜日": row[1],
+                            "勤怠": row[2],
+                            "始業時刻": row[3],
+                            "終業時刻": row[4],
+                            "休憩": row[5],
+                            "勤務時間計": row[6],
+                            "標準時間": row[7],
+                            "平均残業": row[8],
+                            "平日深夜": row[9],
+                            "休日": row[10],
+                            "休日深夜": row[11],
+                            "法定休日": row[12],
+                            "法定深夜": row[13],
+                            "備考": row[14],
                         })
             pdf_df = pd.DataFrame(data)
             print(pdf_df)
