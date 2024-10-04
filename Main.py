8cc899e8d5a639dc9a5ee4e5f544dcbaa92cc890
DESKTOP-F9CLLTD\USER <masa5228@gmail.com>
2024-10-04 18:54:58 +0900

    PDF情報抽出処理をメインに実装

--- Main.py
+++ Main.py
@@ -1,16 +1,16 @@
 import tkinter as tk
 from tkinter import filedialog, messagebox
 
-<<<<<<< HEAD
-=======
+from Higuchi import Higuchi
 from Murase import Murase
 
->>>>>>> origin/main
+
 class MHSystemGUI:
     def __init__(self, master):
         self.master = master
         master.title("MHSystem")
         master.geometry("400x300")
+        self.file_path = None
 
         # メインフレーム
         self.main_frame = tk.Frame(master)
@@ -18,42 +18,45 @@ class MHSystemGUI:
 
         # ファイル選択ボタン
         self.select_file_button = tk.Button(self.main_frame, text="PDFファイルを選択", command=self.select_file)
-        self.select_file_button.pack(pady=10)
+        self.select_file_button.pack(pady=10, anchor='e')
 
         # 実行ボタン
         self.run_button = tk.Button(self.main_frame, text="実行", command=self.run_process)
-        self.run_button.pack(pady=10)
-
+        self.run_button.pack(pady=10, anchor='e')
         # 結果表示エリア
         self.result_text = tk.Text(self.main_frame, height=10, width=50)
         self.result_text.pack(pady=10)
 
+    # ファイル選択ボタンの処理
     def select_file(self):
         file_path = filedialog.askopenfilename(filetypes=[("PDF files", "*.pdf")])
         if file_path:
             self.file_path = file_path
             self.result_text.insert(tk.END, f"選択されたファイル: {file_path}\n")
 
+    # 実行ボタンの処理
     def run_process(self):
-        # ここに処理を実装する
-        	# 会社判別
-        	# 各社　pdf読み込み　→　フォーマット合わせが目的
-        	# ジョブカンデータ読み込み
-        	# 二次元配列→クラスへ
-        	# 最終サニタイズ　→　時刻を丸める（８;５１　→　９：００）
-        	# 比較　完全一致比較
-        	# ファイル名作成　（出向先_氏名_yyyyMMdd.csv）
-        	# クラス→二次元配列
-        	# 出力
-        	
+        if(self.file_path == None):
+            messagebox.showerror("エラー", "ファイルが選択されていません")
+            return
+
+
+        # 会社判別
+        company_code = 1 #ITCROSS
+
+        # 各社　pdf読み込み　→　フォーマット合わせが目的
+        Higuchi.read_pdf(company_code, self.file_path)
+        # ジョブカンデータ読み込み
+        # 二次元配列→クラスへ
+        # 最終サニタイズ　→　時刻を丸める（８;５１　→　９：００）
+        # 比較　完全一致比較
+        # ファイル名作成　（出向先_氏名_yyyyMMdd.csv）
+        # クラス→二次元配列
+        # 出力
+        
         # ポップアップ出力（おわったよ。差異無いよ。三日分違うよ（9/10,9/11,9/12））
         difference_days = ["9/10", "9/11", "9/12"]
-<<<<<<< HEAD
-        output_message = f"処理が完了しました。\n差異{len(difference_days)}件\n{difference_days}"
-        messagebox.showinfo(output_message)
-=======
         messagebox.showinfo(Murase.output_message(difference_days))
->>>>>>> origin/main
 
 
 if __name__ == "__main__":
